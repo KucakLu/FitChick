852ebb11846f1d9562e632b9edfae0047917aab2
HendraaaIrwn <hendraabuhilmi@gmail.com>
2026-05-19 17:23:00 +0700

    Initial Commit

---
+++ FitChick/FitChickApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitChickApp.swift
+//  FitChick
+//
+//  Created by Hendra Irawan on 19/05/26.
+//
+
+import SwiftUI
+
+@main
+struct FitChickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
